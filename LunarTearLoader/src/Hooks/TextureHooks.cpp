cd7885e4c80531bcec1a1da5d673ec599be328a5
ifa-ifa <178672877+ifa-ifa@users.noreply.github.com>
2025-08-10 20:57:47 +0100

    Improved logging, added support for uncompressed formats

--- LunarTearLoader/src/Hooks/TextureHooks.cpp
+++ LunarTearLoader/src/Hooks/TextureHooks.cpp
@@ -34,6 +34,9 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
     size_t ddsFileSize = 0;
     void* ddsFileData = LoadLooseFile(tex_path.string().c_str(), ddsFileSize);
 
+    bool foundByHash = false;
+    std::string hashed_filename;
+
     // If not found, try loading by CRC32c hash (older mods based on SpecialK's injection use this)
     if (!ddsFileData) {
         mipSurface* pMipSurfaces = (mipSurface*)((uintptr_t)&tex->bxonAssetHeader->offsetToMipSurfaces + tex->bxonAssetHeader->offsetToMipSurfaces);
@@ -45,11 +48,13 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
 
             std::stringstream ss;
             ss << std::setfill('0') << std::setw(8) << std::hex << std::uppercase << hash << ".dds";
-            std::string hashed_filename = ss.str();
-
+            hashed_filename = ss.str();
             Logger::Log(Verbose) << "Could not find texture by name. Trying SpecialK hash: " << hashed_filename;
 
             ddsFileData = LoadLooseFile(hashed_filename.c_str(), ddsFileSize);
+            if (ddsFileData) {
+                foundByHash = true;
+            }
         }
     }
 
@@ -58,8 +63,12 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
         return TexHook_original(tex, param_2, param_3);
     }
 
-    Logger::Log(Info) << "Found loose texture file: " << tex_path.string();
-
+    if (foundByHash) {
+        Logger::Log(Info) << "Found loose texture file: " << tex_path.string() << " (" << hashed_filename << ")";
+    }
+    else {
+        Logger::Log(Info) << "Found loose texture file: " << tex_path.string();
+    }
     if (ddsFileSize < sizeof(DDS_HEADER)) {
         Logger::Log(Error) << " | Error: DDS file is too small to be valid.";
         return TexHook_original(tex, param_2, param_3);
@@ -74,23 +83,50 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
     void* pixelDataStart = nullptr;
     size_t pixelDataSize = 0;
 
-    if (ddsHeader->ddspf_dwFourCC == '01XD') {
+    if (ddsHeader->ddspf_dwFourCC == '01XD') { // "DX10"
         Logger::Log(Verbose) << " | DX10 header detected.";
-
         DDS_HEADER_DXT10* dx10Header = (DDS_HEADER_DXT10*)((char*)ddsFileData + sizeof(DDS_HEADER));
         finalFormat = DXGIFormatToXonFormat(dx10Header->dxgiFormat);
         pixelDataStart = (char*)dx10Header + sizeof(DDS_HEADER_DXT10);
         pixelDataSize = ddsFileSize - (sizeof(DDS_HEADER) + sizeof(DDS_HEADER_DXT10));
     }
-    else {
-        Logger::Log(Verbose) << " | Legacy header detected.";
+    else if (ddsHeader->ddspf_dwFlags & DDPF_RGB) {
+        Logger::Log(Verbose) << " | Uncompressed RGB header detected.";
+        // Check for 32bpp BGRA (a very common format)
+        if (ddsHeader->ddspf_dwRGBBitCount == 32 &&
+            ddsHeader->ddspf_dwRBitMask == 0x00FF0000 &&
+            ddsHeader->ddspf_dwGBitMask == 0x0000FF00 &&
+            ddsHeader->ddspf_dwBBitMask == 0x000000FF)
+        {
+            finalFormat = R8G8B8A8_UNORM;
+        }
+        // Check for 32bpp RGBA
+        else if (ddsHeader->ddspf_dwRGBBitCount == 32 &&
+            ddsHeader->ddspf_dwRBitMask == 0x000000FF &&
+            ddsHeader->ddspf_dwGBitMask == 0x0000FF00 &&
+            ddsHeader->ddspf_dwBBitMask == 0x00FF0000)
+        {
+            finalFormat = R8G8B8A8_UNORM;
+        }
+        pixelDataStart = (char*)ddsHeader + sizeof(DDS_HEADER);
+        pixelDataSize = ddsFileSize - sizeof(DDS_HEADER);
+    }
+    else if (ddsHeader->ddspf_dwFlags & DDPF_FOURCC) {
+        Logger::Log(Verbose) << " | Legacy FourCC header detected.";
         finalFormat = FourCCToXonFormat(ddsHeader->ddspf_dwFourCC);
         pixelDataStart = (char*)ddsHeader + sizeof(DDS_HEADER);
         pixelDataSize = ddsFileSize - sizeof(DDS_HEADER);
     }
 
     if (finalFormat == UNKNOWN) {
-        Logger::Log(Error) << " | Unsupported DDS pixel format in loose file.";
+        if (ddsHeader->ddspf_dwFlags & DDPF_RGB) {
+            Logger::Log(Error) << " | Unsupported DDS pixel format in loose file. Unrecognized uncompressed (RGB) format with bitcount: " << ddsHeader->ddspf_dwRGBBitCount;
+        }
+        else {
+            char fourCCStr[5] = { 0 };
+            memcpy(fourCCStr, &ddsHeader->ddspf_dwFourCC, 4);
+            Logger::Log(Error) << " | Unsupported DDS pixel format in loose file - Unrecognized FourCC: '" << fourCCStr << "'";
+        }
         return TexHook_original(tex, param_2, param_3);
     }
 
@@ -100,6 +136,11 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
         return TexHook_original(tex, param_2, param_3);
     }
 
+    if (ddsHeader->dwWidth != tex->bxonAssetHeader->texWidth || ddsHeader->dwHeight != tex->bxonAssetHeader->texHeight) {
+        Logger::Log(Error) << " | Loose DDS has different dimensions (" << ddsHeader->dwWidth << "x" << ddsHeader->dwHeight
+            << ") than original texture (" << tex->bxonAssetHeader->texWidth << "x" << tex->bxonAssetHeader->texHeight << "). Mismatched resolutions are not supported.";
+        return TexHook_original(tex, param_2, param_3);
+    }
 
     tex->texData = pixelDataStart; // game uses arena allocator, unlikely to try free this pointer
     tex->texDataSize = pixelDataSize;
@@ -115,12 +156,46 @@ uint64_t TexHook_detoured(tpgxResTexture* tex, void* param_2, void* param_3) {
     uint32_t currentWidth = ddsHeader->dwWidth;
     uint32_t currentHeight = ddsHeader->dwHeight;
 
+    bool isBlockCompressed = false;
+    uint32_t bytesPerPixel = 0;
+
+    switch (finalFormat) {
+    case BC1_UNORM:
+    case BC1_UNORM_SRGB:
+    case BC2_UNORM:
+    case BC2_UNORM_SRGB:
+    case BC3_UNORM:
+    case BC3_UNORM_SRGB:
+    case BC4_UNORM:
+    case BC5_UNORM:
+    case BC7_UNORM:
+        isBlockCompressed = true;
+        break;
+    case R8G8B8A8_UNORM_STRAIGHT:
+    case R8G8B8A8_UNORM:
+    case R8G8B8A8_UNORM_SRGB:
+        bytesPerPixel = 4;
+        break;
+        // Add other uncompressed formats here if needed
+    default:
+        Logger::Log(Error) << "Cannot rebuild mip surfaces: Unknown format size for " << XonFormatToString(finalFormat);
+        return TexHook_original(tex, param_2, param_3); // Abort if we don't know the size
+    }
+
+
     for (uint32_t i = 0; i < ddsHeader->dwMipMapCount; i++) {
-        bool is_8_byte_format = (finalFormat == BC1_UNORM || finalFormat == BC1_UNORM_SRGB || finalFormat == BC4_UNORM);
-        uint32_t bytesPerBlock = is_8_byte_format ? 8 : 16;
         uint32_t mipWidth = std::max(1u, currentWidth);
         uint32_t mipHeight = std::max(1u, currentHeight);
-        uint32_t mipSize = ((mipWidth + 3) / 4) * ((mipHeight + 3) / 4) * bytesPerBlock;
+        uint32_t mipSize = 0;
+
+        if (isBlockCompressed) {
+            bool is_8_byte_format = (finalFormat == BC1_UNORM || finalFormat == BC1_UNORM_SRGB || finalFormat == BC4_UNORM);
+            uint32_t bytesPerBlock = is_8_byte_format ? 8 : 16;
+            mipSize = ((mipWidth + 3) / 4) * ((mipHeight + 3) / 4) * bytesPerBlock;
+        }
+        else {
+            mipSize = mipWidth * mipHeight * bytesPerPixel;
+        }
 
         pMipSurfaces[i].offset = currentOffset;
         pMipSurfaces[i].size = mipSize;
