95cd00991f0e45d4d20e63e6a73d61d40e89bb80
ifa-ifa <178672877+ifa-ifa@users.noreply.github.com>
2025-12-28 19:59:22 +0000

    replaced sha356 exe check with crc32c

--- LunarTearLoader/src/Common/SHA256.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-std::wstring BytesToHex(const unsigned char* data, size_t len);
-bool HashFileSHA256(const std::wstring& path, std::vector<unsigned char>& outHash);
--- LunarTearLoader/src/DLLMain.cpp
+++ LunarTearLoader/src/DLLMain.cpp
@@ -1,11 +1,10 @@
 ﻿#include <Windows.h>
-#include "ModLoader.h"
-#include <timeapi.h> 
-#include "Common/SHA256.h"
 #include <filesystem>
 #include <format>
-#pragma comment(lib, "winmm.lib") 
 #include "Init.h"
+#include <fstream>
+#include "ModLoader.h"
+#include <crc32c/crc32c.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
@@ -15,30 +14,26 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
         wchar_t processPath[MAX_PATH];
         GetModuleFileNameW(NULL, processPath, MAX_PATH);
 
-        std::vector<unsigned char> currentHash;
-        if (!HashFileSHA256(processPath, currentHash)) {
-            MessageBoxW(NULL, L"Fatal error - HashFileSHA256 Failed", L"Lunar Tear", MB_OK);
-            return FALSE;
-        }
+        std::ifstream file(processPath, std::ios::binary | std::ios::ate);
+        std::streamsize size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        std::vector<char> buffer(size);
 
-        static const unsigned char expectedHash[32] = {
-            0x90,0x76,0x93,0xCB,0x5E,0x03,0xC6,0x1D,0xD4,0x22,0xE8,0xEE,0xAD,0xF1,0xCE,0x73,
-            0xF3,0x12,0x2A,0x6C,0xA4,0xE5,0x07,0x99,0xBF,0xC7,0x36,0xBF,0x83,0x9E,0xED,0x16
-        };
+        if (file.read(buffer.data(), size)) {
+            uint32_t currentHash = crc32c::Crc32c(buffer.data(), size);
+            const uint32_t expectedHash = 396277532;
 
-        if (memcmp(expectedHash, currentHash.data(), 32) != 0) {
-            std::wstring expectedHex = BytesToHex(expectedHash, 32);
-            std::wstring actualHex = BytesToHex(currentHash.data(), currentHash.size());
+            if (currentHash != expectedHash) {
+                int result = MessageBoxW(
+                    NULL,
+                    std::format(L"CRC32c mismatch - Expected: {:#x}, Actual: {:#x}\n\nYour game executable may be modified, corrupted, or on the wrong game version. Expect Lunar Tear to not work properly. Continue anyway?", expectedHash, currentHash).c_str(),
+                    L"Lunar Tear",
+                    MB_OKCANCEL
+                );
+                if (result != IDOK) {
 
-            int result = MessageBoxW(
-                NULL,
-                std::format(L"SHA256 mismatch - Expected: {}, Actual: {}\n\nYour game executable may be modified, corrupted, or on the wrong game version. Expect Lunar Tear to not work properly. Continue anyway?", expectedHex, actualHex).c_str(),
-                L"Lunar Tear",
-                MB_OKCANCEL
-            );
-            if (result != IDOK) {
-
-                return FALSE;
+                    return FALSE;
+                }
             }
         }
         
